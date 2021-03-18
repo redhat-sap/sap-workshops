402fd8fe75be7a4133f2d7fea99059377efe463e
Markus Koch <mkoch@redhat.com>
2021-03-18 17:01:11 +0100

    update

--- sap-enablement-ansible/Lab/create_readme.sh
+++ sap-enablement-ansible/Lab/create_readme.sh
@@ -3,5 +3,8 @@
 asciidoctor -b docbook5 index.adoc
 # convert docbook to markdown
 pandoc -f docbook -t markdown_strict index.xml -o ../README.md
-
 rm index.xml
+
+sed -i -e 's/\(\[e2e-infra-layout\]\).*/\1\(img\/infra_layout.png\)/g' ../README.md
+sed -i -e 's/\(\[rhpds-catalog]\).*/\1\(img\/rhpds01.png\)/g'  ../README.md
+sed -i -e 's/\(\[rhpds-submit]\).*/\1\(img\/rhpds02.png\)/g'  ../README.md
